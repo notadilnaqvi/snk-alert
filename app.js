77c3b7ae5fd222bc2437f545fadefa6d15a949c8
Adil Naqvi <notadilnaqvi@gmail.com>
2020-09-05 23:30:56 +0500

    Testing

--- app.js
+++ app.js
@@ -3,39 +3,57 @@ const moment = require('moment');
 const fetch = require('node-fetch');
 const nodemailer = require("nodemailer");
 
-URL = "https://api.reddit.com/user/SNKbot/submitted?limit=2"
+const URL = "https://api.reddit.com/user/sahnbk/submitted?limit=1"
 
-let re = /\[New Chapter Spoilers\] Chapter \d\d\d RELEASE Megathread!/;
+const re = /\[New Chapter Spoilers\] Chapter \d\d\d RELEASE Megathread!/;
+let oldPost = "";
 
-(async () => {
-    let response = await fetch(URL);
-    json = await response.json();
-    json.data.children.forEach(child => {
-        if(child.data.title.match(re) && child.data.link_flair_text == "Latest Chapter"){
-            sendEmail(child.data.url);
-        }
-    });
-})();
+setInterval(() => {
+    (async () => {
+        let response = await fetch(URL);
+        json = await response.json();
+        
+        json.data.children.forEach(child => {
+            if(child.data.name != oldPost && child.data.title.match(re)){
+                // sendEmail(child.data.url);
+                console.log("New chapter: " + child.data.name);
+                oldPost = child.data.name;
+            } else {
+                console.log("Nothing new...");
+            }
+        });
+    })();
+}, 10000);
+// (async () => {
+//     let response = await fetch(URL);
+//     json = await response.json();
+//     json.data.children.forEach(child => {
+//         if(child.data.name != oldPost && child.data.title.match(re) && child.data.link_flair_text == "Latest Chapter"){
+//             // sendEmail(child.data.url);
+//             console.log(child.data.name);
+//             oldPost = child.data.name;
+//         }
+//     });
+// })();
 
+// function sendEmail(_url) {
 
-function sendEmail(_url) {
+//     let transporter = nodemailer.createTransport({
+//         host: 'smtp.gmail.com',
+//         port: 465,
+//         secure: true,
+//         auth: {
+//             user: process.env.GMAIL_USER,
+//             pass:  process.env.GMAIL_PASSWORD,
+//         },
+//     });
 
-    let transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-            user: process.env.GMAIL_USER, // generated ethereal user
-            pass:  process.env.GMAIL_PASSWORD, // generated ethereal password
-        },
-    });
+//     const mailOptions = {
+//         from: process.env.GMAIL_USER,
+//         to: process.env.GMAIL_RECIPIENT,
+//         subject: "New SNK Chapter",
+//         html: `Oi,<br><br>A new ⚔️ SNK ⚔️ chapter just dropped. Go read at <a style="text-decoration: none" href = ${_url}>this link</a>. Thank me later!<br><br><a style="text-decoration: none" href = "https://github.com/notadilnaqvi/snk-alert">– SNK Alert Bot</a>`
+//     }
 
-    const mailOptions = {
-        from: "notadilnaqvi@gmail.com", // sender address
-        to: "olz56908@cuoly.com", // list of receivers
-        subject: "New SNK Chapter", // Subject line
-        html: `Oi,<br><br>A new ⚔️ SNK ⚔️ chapter just dropped. Go read at <a style="text-decoration: none" href = ${_url}>this link</a>. Thank me later!<br><br>–Adil<br><a style="text-decoration: none" href = "https://github.com/notadilnaqvi/snk-alert">SNK Alert Github Repo</a>`
-    }
-
-    transporter.sendMail(mailOptions);
-}
+//     transporter.sendMail(mailOptions);
+// }
